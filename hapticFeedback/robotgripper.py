a425f61631707ee5e4fa30a096ae981dc1854fc9
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2025-09-12 17:30:20 +0200

    Update robotgripper.py

--- hapticFeedback/robotgripper.py
+++ hapticFeedback/robotgripper.py
@@ -89,7 +89,6 @@ async def main():
                 if (angle_int >= 85 and angle_int <= 95):
                     l=0
                     r=0
-
                 send(l,r,0,0)
                 print(f"➡️ Servo angle gestuurd: {angle_int}")
                 time.sleep(0.5)
